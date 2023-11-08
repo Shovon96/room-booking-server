d16bd005e69c07b3d713a697f944e083b993671f
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-08 14:31:45 +0600

    coockie verified

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(express.json());
 app.use(cookieParser())
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
